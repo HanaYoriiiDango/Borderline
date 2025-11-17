65065c5c093ec770ba70ada7078988fe7f6c37c4
HanaYoriDango <HanaYoriDango@DESKTOP-JFUIT64>
2025-11-17 07:14:08 +0700

    checkl

--- TestScale/Global.cpp
+++ TestScale/Global.cpp
@@ -4,5 +4,5 @@
 vector<Emotion_> Emotion{ JOY, SADNESS, POWER, FEAR, CALM, ANGER };
 string Emotion_Names[COUNT_Emotions] = { "Радость", "Грусть", "Сила", "Страх", "Спокойствие", "Гнев" };
 string Worlds_Names[COUNT_Emotions] = { "Мир Радости", "Мир Грусти", "Мир Силы", "Мир Страха", "Мир Спокойствия", "Мир Гнева" };
-Player Hero;
+Player Hero; 
 Location Worlds[COUNT_Emotions];
